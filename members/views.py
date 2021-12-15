9ca3078eebef196a9eacba5cb5bfb58052d4f150
EunCHanPark <mango3392@icloud.com>
2021-12-15 17:34:15 +0900

    페이지 생성
    페이지에 이름 이메일 나오게 하기

--- members/views.py
+++ members/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from .models import Members
 # Create your views here.
 
 def index(request):
+    members = Members.objects.all()
+
     return render(
         request,
         'members/index.html',
-                  )
+        {
+            'members': members,
+        }
+)
